cab52cc2ff2add5eab1b4a259be8ad215a9c4f71
Andreas Hubert <anhubert@gmail.com>
2019-03-31 15:14:26 +0200

    add docker relevant options

--- roles/k3s/molecule/default/tests/test_default.py
+++ roles/k3s/molecule/default/tests/test_default.py
@@ -6,9 +6,10 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
+def test_cmdline_txt_file(host):
+    f = host.file('/boot/cmdline.txt')
 
     assert f.exists
+    assert f.content == b'dwc_otg.lpm_enable=0 console=serial0,115200 console=tty1 root=PARTUUID=6af5c44e-02 rootfstype=ext4 elevator=deadline fsck.repair=yes rootwait cgroup_memory=1 cgroup_enable=memory'
     assert f.user == 'root'
     assert f.group == 'root'
